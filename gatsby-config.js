ca1fb2938a6082095ba6419f132c81c60f322abf
kmmok <mandymok@duck.com>
2022-11-27 08:49:55 +1300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Life is like a box of chocolates`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
